2a2388eac095adeedfdabe589e7593d9bd379860
Changyu Seon <seonchg@gmail.com>
2018-02-11 00:41:43 +0900

    modify some configuration for docker

--- initial/src/main/java/hello/Application.java
+++ initial/src/main/java/hello/Application.java
@@ -7,7 +7,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class Application {
+
+    @RequestMapping("/")
+    public String home() {
+        return "Hello Docker World";
+    }
+
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
